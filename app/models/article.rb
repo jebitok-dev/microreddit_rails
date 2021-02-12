551e65430e1c8a78f37f8e30a3cf7c50905302db
Jebitok <sharonkosgei4@gmail.com>
2021-02-12 15:20:27 +0300

    add changes to article &comment models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
+  belongs_to :article
+
   validates :link, presence: true, length: { maximum: 200 }
   validates :author, presence: true, uniqueness: true
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :article
+
   validates :comment, presence: true, length: { maximum: 100 }
   validates :author, presence: true, uniqueness: true
 end
